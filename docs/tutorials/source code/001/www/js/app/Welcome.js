8deb9f49d70bce766392ce322bfbdf393edf43e4
lordking <lordking@163.com>
2013-11-20 15:55:21 +0800

    模块编程起步教程

---
+++ docs/tutorials/source code/001/www/js/app/Welcome.js
@@ -0,0 +1,16 @@
+
+/*
+ * Author: U{jinlei<mailto:jinlei3@asiainfo-linkage.com>}
+ * Date: 2013-11-20
+ * Copyright: asiainfo-linkage
+ * Version: $Revision$
+ */
+
+define([],function(){
+
+	function msg() {
+		return "欢迎加入javascript模块化编程的队伍"
+	}
+
+	return msg;
+})
---
+++ docs/tutorials/source code/001/www/js/main.js
@@ -0,0 +1,22 @@
+
+/*
+ * Author: U{jinlei<mailto:jinlei3@asiainfo-linkage.com>}
+ * Date: 2013-11-20
+ * Copyright: asiainfo-linkage
+ * Version: $Revision$
+ */
+
+require.config({
+	baseUrl : "js",
+	paths : {
+		jquery : 'lib/jquery'
+	}
+});
+
+require([
+	'jquery',
+	'app/Welcome'
+],function(jquery, Welcome){
+
+	$('#welcome').html(Welcome);
+})
